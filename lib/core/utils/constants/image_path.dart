498a23eb4c4a7509aab1b6cdf34c91edebb35f55
Yasin Mia palash <yasinmiapolash@gmail.com>
2024-11-26 13:35:03 +0600

    Update image_path.dart

--- lib/core/utils/constants/image_path.dart
+++ lib/core/utils/constants/image_path.dart
@@ -1,4 +1,6 @@
 
 class ImagePath{
-  static const  String logo='';
+   static const  String _basePath='assets/images';
+  static const  String backGroundImage='$_basePath/backgroundImage.png';
+  static const  String appLogo='assets/logos/appLogo.png';
 }
