92f9a40f4089c08ae90e526973adb68f60fa33a1
Yasin Mia palash <yasinmiapolash@gmail.com>
2024-11-26 13:31:03 +0600

    Update global_text_style.dart

--- lib/core/common/styles/global_text_style.dart
+++ lib/core/common/styles/global_text_style.dart
@@ -3,17 +3,31 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 
-TextStyle getTextStyle({
-  double fontSize = 14.0,
-  FontWeight fontWeight = FontWeight.w400,
-  double lineHeight = 21.0,
-  TextAlign textAlign = TextAlign.center,
-  Color color = Colors.black,
-}) {
-  return GoogleFonts.poppins(
-    fontSize: fontSize.sp,
-    fontWeight: fontWeight,
-    height: fontSize.sp/ lineHeight.sp,
-    color: color,
-  );
-}
+// TextStyle getTextStyle({
+//   double fontSize = 14.0,
+//   FontWeight fontWeight = FontWeight.w400,
+//   double lineHeight = 21.0,
+//   TextAlign textAlign = TextAlign.center,
+//   Color color = Colors.black,
+// }) {
+//   return GoogleFonts.poppins(
+//     fontSize: fontSize.sp,
+//     fontWeight: fontWeight,
+//     height: fontSize.sp/ lineHeight.sp,
+//     color: color,
+//   );
+// }
+// TextStyle getTextStyleBt({
+//   double fontSize = 14.0,
+//   FontWeight fontWeight = FontWeight.w400,
+//   double lineHeight = 21.0,
+//   Color color = Colors.black,
+// }) {
+//   return TextStyle(
+//     fontFamily: 'FuturaHv',
+//     fontSize: fontSize.sp,
+//     fontWeight: fontWeight,
+//     height: lineHeight.h / fontSize.sp,
+//     color: color,
+//   );
+// }
