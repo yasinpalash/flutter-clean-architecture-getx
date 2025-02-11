dba036139b6ee7949de4d497db0ac2528bf247c2
yasinpalash <yasinmiapolash@gmail.com>
2025-02-11 13:33:53 +0600

    Refactor app layout for responsive design using Sizer, remove flutter_screenutil dependency, and streamline utility functions

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' as foundation;
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:project_structure_temp/core/bindings/controller_binder.dart';
+import 'package:project_structure_temp/core/utils/constants/app_sizer.dart';
 import 'package:project_structure_temp/routes/app_routes.dart';
 import 'core/utils/theme/theme.dart';
 
-// Utility for Platform Checks
+
 class PlatformUtils {
-  static bool get isIOS => foundation.defaultTargetPlatform == TargetPlatform.iOS;
-  static bool get isAndroid => foundation.defaultTargetPlatform == TargetPlatform.android;
+  static bool get isIOS =>
+      foundation.defaultTargetPlatform == TargetPlatform.iOS;
+  static bool get isAndroid =>
+      foundation.defaultTargetPlatform == TargetPlatform.android;
 }
 
 class MyApp extends StatelessWidget {
@@ -18,34 +20,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(375, 812),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (context, child) => GetMaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute: AppRoute.loginScreen,
-        getPages: AppRoute.routes,
-        initialBinding: ControllerBinder(),
-        themeMode: ThemeMode.system,
-        theme: _getLightTheme(),
-        darkTheme: _getDarkTheme(),
-        defaultTransition: PlatformUtils.isIOS ? Transition.cupertino : Transition.fade,
-        locale: Get.deviceLocale,
-        builder: (context, child) => PlatformUtils.isIOS 
-            ? CupertinoTheme(data: const CupertinoThemeData(), child: child!)
-            : child!,
-      ),
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          initialRoute: AppRoute.loginScreen,
+          getPages: AppRoute.routes,
+          initialBinding: ControllerBinder(),
+          themeMode: ThemeMode.system,
+          theme: _getLightTheme(),
+          darkTheme: _getDarkTheme(),
+          defaultTransition:
+              PlatformUtils.isIOS ? Transition.cupertino : Transition.fade,
+          locale: Get.deviceLocale,
+          builder: (context, child) => PlatformUtils.isIOS
+              ? CupertinoTheme(data: const CupertinoThemeData(), child: child!)
+              : child!,
+        );
+      },
     );
   }
-
 
   ThemeData _getLightTheme() {
     return PlatformUtils.isIOS
         ? AppTheme.lightTheme.copyWith(platform: TargetPlatform.iOS)
         : AppTheme.lightTheme;
   }
-
 
   ThemeData _getDarkTheme() {
     return PlatformUtils.isIOS
--- lib/core/utils/constants/app_sizer.dart
+++ lib/core/utils/constants/app_sizer.dart
@@ -1,49 +1,33 @@
 import 'package:flutter/material.dart';
 
-// Constants for design dimensions
 const double kFigmaDesignWidth = 390;
 const double kFigmaDesignHeight = 844;
 const double kFigmaDesignStatusBar = 0;
 
-// Enum to represent different device types
 enum DeviceType { mobile, tablet, desktop }
 
-// Typedef for responsive build function
 typedef ResponsiveBuild = Widget Function(
     BuildContext context,
     Orientation orientation,
     DeviceType deviceType,
     );
 
-// Extension for responsive scaling
+
 extension ResponsiveExtension on num {
-  /// Scales width relative to the design width
   double get w => (this * SizeUtils.width) / kFigmaDesignWidth;
-
-  /// Scales height relative to the design height
   double get h => (this * SizeUtils.height) / kFigmaDesignHeight;
-
-  /// Scales font size relative to the design width
-  double get fSize => (this * SizeUtils.width) / kFigmaDesignWidth;
+  double get sp => (this * SizeUtils.width) / kFigmaDesignWidth;
 }
 
-// Extension for formatting double values
+
 extension FormatExtension on double {
-  /// Returns a fixed precision value
   double toFixed(int fractionDigits) => double.parse(toStringAsFixed(fractionDigits));
-
-  /// Returns the value if non-zero, otherwise a default value
   double nonZero({double defaultValue = 0.0}) => this > 0 ? this : defaultValue;
-
-  /// Returns the value as a formatted string
   String toFormattedString({int fractionDigits = 2}) => toStringAsFixed(fractionDigits);
 }
 
-// Sizer widget to handle responsive layout
 class Sizer extends StatelessWidget {
   const Sizer({super.key, required this.builder});
-
-  /// Builds the widget whenever the orientation or device type changes
   final ResponsiveBuild builder;
 
   @override
@@ -52,9 +36,7 @@ class Sizer extends StatelessWidget {
       builder: (context, constraints) {
         return OrientationBuilder(
           builder: (context, orientation) {
-            // Set the screen size and device type based on the constraints and orientation
             SizeUtils.setScreenSize(constraints, orientation, context);
-
             return builder(
               context,
               orientation,
@@ -67,30 +49,14 @@ class Sizer extends StatelessWidget {
   }
 }
 
-// Utility class for handling screen size and device type
 class SizeUtils {
-  /// Device's BoxConstraints
   static late BoxConstraints boxConstraints;
-
-  /// Device's Orientation
   static late Orientation orientation;
-
-  /// Type of Device (mobile, tablet, or desktop)
   static late DeviceType deviceType;
-
-  /// Device's Width
   static late double width;
-
-  /// Device's Height
   static late double height;
-
-  /// Safe Area Padding (for notches, status bar, etc.)
   static late EdgeInsets safeAreaPadding;
-
-  /// Debug mode flag for printing debug info
   static bool debugMode = false;
-
-  /// Sets the screen size and calculates width, height, and device type
   static void setScreenSize(
       BoxConstraints constraints,
       Orientation currentOrientation,
@@ -107,8 +73,6 @@ class SizeUtils {
       width = boxConstraints.maxHeight.nonZero(defaultValue: kFigmaDesignWidth);
       height = boxConstraints.maxWidth.nonZero();
     }
-
-    // Determine the device type based on screen width
     if (width >= 1200) {
       deviceType = DeviceType.desktop;
     } else if (width >= 600) {
@@ -116,11 +80,9 @@ class SizeUtils {
     } else {
       deviceType = DeviceType.mobile;
     }
-
     if (debugMode) debugPrintInfo();
   }
 
-  /// Prints debug information about the current screen size
   static void debugPrintInfo() {
     debugPrint("Device Width: $width");
     debugPrint("Device Height: $height");
@@ -128,8 +90,6 @@ class SizeUtils {
     debugPrint("Device Type: $deviceType");
     debugPrint("Orientation: $orientation");
   }
-
-  /// Utility for adaptive padding based on device type
   static double adaptivePadding({
     required double mobile,
     required double tablet,
@@ -144,10 +104,6 @@ class SizeUtils {
         return mobile;
     }
   }
-
-  /// Checks if the orientation is portrait
   static bool get isPortrait => orientation == Orientation.portrait;
-
-  /// Checks if the orientation is landscape
   static bool get isLandscape => orientation == Orientation.landscape;
 }
