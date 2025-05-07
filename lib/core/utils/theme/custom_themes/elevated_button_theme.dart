072649867400d32d25af2036b72520dc3af0d3db
Yasin Mia palash <yasinmiapolash@gmail.com>
2025-05-07 16:24:23 +0600

    Update elevated_button_theme.dart

--- lib/core/utils/theme/custom_themes/elevated_button_theme.dart
+++ lib/core/utils/theme/custom_themes/elevated_button_theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_structure_temp/core/utils/constants/app_sizer.dart';
 
 class AppElevatedButtonTheme {
   AppElevatedButtonTheme._();
@@ -9,23 +10,25 @@ class AppElevatedButtonTheme {
     required Color defaultBackgroundColor,
     required Color disabledBackgroundColor,
     required Color borderColor,
+    required EdgeInsetsGeometry padding,
+    required BorderRadius borderRadius,
   }) {
     return ElevatedButtonThemeData(
       style: ButtonStyle(
         elevation: const WidgetStatePropertyAll(0),
         foregroundColor: WidgetStateProperty.resolveWith<Color>(
-          (states) => states.contains(WidgetState.disabled) ? disabledTextColor : defaultTextColor,
+              (states) => states.contains(WidgetState.disabled) ? disabledTextColor : defaultTextColor,
         ),
         backgroundColor: WidgetStateProperty.resolveWith<Color>(
-          (states) => states.contains(WidgetState.disabled) ? disabledBackgroundColor : defaultBackgroundColor,
+              (states) => states.contains(WidgetState.disabled) ? disabledBackgroundColor : defaultBackgroundColor,
         ),
         side: WidgetStateProperty.all(BorderSide(color: borderColor)),
-        padding: const WidgetStatePropertyAll(EdgeInsets.symmetric(vertical: 18)),
-        textStyle: const WidgetStatePropertyAll(
-          TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+        padding: WidgetStatePropertyAll(padding),
+        textStyle:  WidgetStatePropertyAll(
+          TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w600),
         ),
         shape: WidgetStateProperty.all(
-          RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          RoundedRectangleBorder(borderRadius: borderRadius),
         ),
       ),
     );
@@ -37,6 +40,8 @@ class AppElevatedButtonTheme {
     defaultBackgroundColor: Colors.blue,
     disabledBackgroundColor: Colors.grey.shade300,
     borderColor: Colors.blue,
+    padding:  EdgeInsets.symmetric(vertical: 18.h),
+    borderRadius: BorderRadius.circular(12.h),
   );
 
   static final ElevatedButtonThemeData darkElevatedButtonTheme = _baseTheme(
@@ -45,5 +50,7 @@ class AppElevatedButtonTheme {
     defaultBackgroundColor: Colors.blueGrey,
     disabledBackgroundColor: Colors.grey.shade800,
     borderColor: Colors.blueGrey,
+    padding:  EdgeInsets.symmetric(vertical: 18.h),
+    borderRadius: BorderRadius.circular(12.h),
   );
 }
