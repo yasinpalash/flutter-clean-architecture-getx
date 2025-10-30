f4b6d5d3b5e915481a19a8073737ed79a92f633f
Yasin Mia palash <yasinmiapolash@gmail.com>
2025-10-30 16:58:54 +0600

    chore: Clean up whitespace and formatting

--- lib/core/localization/app_localizations.dart
+++ lib/core/localization/app_localizations.dart
@@ -3,8 +3,6 @@ import 'package:get/get.dart';
 class AppTranslations extends Translations {
   @override
   Map<String, Map<String, String>> get keys => {
-    'en_US': {
-
-    },
-  };
+        'en_US': {},
+      };
 }
